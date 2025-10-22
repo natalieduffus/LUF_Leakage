e5f25c94accc923c39a789fbbc98f77651279f44
natalieduffus <99899842+natalieduffus@users.noreply.github.com>
2025-10-22 15:00:26 +0100

    Made init script

---
+++ in_LUF.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=LUF # the name for the cluster scheduler
+#SBATCH --time=00:30:00 # Maximum allowed runtime per iteration
+#SBATCH --mem-per-cpu=7G
+#SBATCH --array=1-53 # the number of iterations
+#SBATCH --output=logfiles/LUF_%A_%a.out # the name of the output files
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=natalie.duffus@biology.ox.ac.uk
+
+# load the anaconda module
+module load Anaconda3
+
+# Make a string variable corresponding to your $DATA directory, then conda_envs/nats_modelling. E.g. for Dave the absolute path is /data/zool-mosquito_ecology/zool2291/conda_envs/nats_modelling
+
+export CONPREFIX=$DATA/conda_envs/nats_modelling
+
+# activate your new conda environment
+source activate $CONPREFIX
+
+Rscript LUF_FLAME_v1.R ${SLURM_ARRAY_TASK_ID}
